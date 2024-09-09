a23ba93d1f46c18cf92c7e9888008e72c73e855c
Stefano Pigozzi <me@steffo.eu>
2024-09-09 19:08:18 +0200

    Port to UV

--- nortrom/__main__.py
+++ nortrom/__main__.py
@@ -7,93 +7,98 @@ import logging
 logging.basicConfig(level="DEBUG")
 
 
-bot = dec.Bot(command_prefix="!", intents=d.Intents(guilds=True, members=True, voice_states=True))
-sbot = ds.SlashCommand(client=bot, sync_commands=True)
+def main():
+    bot = dec.Bot(command_prefix="!", intents=d.Intents(guilds=True, members=True, voice_states=True))
+    sbot = ds.SlashCommand(client=bot, sync_commands=True)
 
 
-@sbot.slash(name="mute", description="Mute all members in the voice channel you're in.")
-async def _mute(ctx: ds.SlashContext):
-    voice: t.Optional[d.VoiceState] = ctx.author.voice
-    if voice is None:
-        await ctx.send(content="⚠️ You're not in a voice channel.", hidden=True)
-        return
+    @sbot.slash(name="mute", description="Mute all members in the voice channel you're in.")
+    async def _mute(ctx: ds.SlashContext):
+        voice: t.Optional[d.VoiceState] = ctx.author.voice
+        if voice is None:
+            await ctx.send(content="⚠️ You're not in a voice channel.", hidden=True)
+            return
 
-    channel: d.VoiceChannel = voice.channel
+        channel: d.VoiceChannel = voice.channel
 
-    permissions: d.Permissions = ctx.author.permissions_in(channel=channel)
-    if not permissions.mute_members:
-        await ctx.send(content="⚠️ You don't have the _Mute Members_ permission.", hidden=True)
-        return
+        permissions: d.Permissions = ctx.author.permissions_in(channel=channel)
+        if not permissions.mute_members:
+            await ctx.send(content="⚠️ You don't have the _Mute Members_ permission.", hidden=True)
+            return
 
-    members: list[d.Member] = channel.members
-    for member in members:
-        await member.edit(mute=True)
+        members: list[d.Member] = channel.members
+        for member in members:
+            await member.edit(mute=True)
 
-    await ctx.send(content="🔇 Speak not.")
-
-
-@sbot.slash(name="unmute", description="Unmute all members in the voice channel you're in.")
-async def _unmute(ctx: ds.SlashContext):
-    voice: t.Optional[d.VoiceState] = ctx.author.voice
-    if voice is None:
-        await ctx.send(content="⚠️ You're not in a voice channel.", hidden=True)
-        return
-
-    channel: d.VoiceChannel = voice.channel
-
-    permissions: d.Permissions = ctx.author.permissions_in(channel=channel)
-    if not permissions.mute_members:
-        await ctx.send(content="⚠️ You don't have the _Mute Members_ permission.", hidden=True)
-        return
-
-    members: list[d.Member] = channel.members
-    for member in members:
-        await member.edit(mute=False)
-
-    await ctx.send(content="🔊 Speak your last.")
+        await ctx.send(content="🔇 Speak not.")
 
 
-@sbot.slash(name="deafen", description="Deafen all members in the voice channel you're in.")
-async def _deafen(ctx: ds.SlashContext):
-    voice: t.Optional[d.VoiceState] = ctx.author.voice
-    if voice is None:
-        await ctx.send(content="⚠️ You're not in a voice channel.", hidden=True)
-        return
+    @sbot.slash(name="unmute", description="Unmute all members in the voice channel you're in.")
+    async def _unmute(ctx: ds.SlashContext):
+        voice: t.Optional[d.VoiceState] = ctx.author.voice
+        if voice is None:
+            await ctx.send(content="⚠️ You're not in a voice channel.", hidden=True)
+            return
 
-    channel: d.VoiceChannel = voice.channel
+        channel: d.VoiceChannel = voice.channel
 
-    permissions: d.Permissions = ctx.author.permissions_in(channel=channel)
-    if not permissions.deafen_members:
-        await ctx.send(content="⚠️ You don't have the _Deafen Members_ permission.", hidden=True)
-        return
+        permissions: d.Permissions = ctx.author.permissions_in(channel=channel)
+        if not permissions.mute_members:
+            await ctx.send(content="⚠️ You don't have the _Mute Members_ permission.", hidden=True)
+            return
 
-    members: list[d.Member] = channel.members
-    for member in members:
-        await member.edit(deafen=True)
+        members: list[d.Member] = channel.members
+        for member in members:
+            await member.edit(mute=False)
 
-    await ctx.send(content="🔇 Hear not.")
-
-
-@sbot.slash(name="undeafen", description="Undeafen all members in the voice channel you're in.")
-async def _undeafen(ctx: ds.SlashContext):
-    voice: t.Optional[d.VoiceState] = ctx.author.voice
-    if voice is None:
-        await ctx.send(content="⚠️ You're not in a voice channel.", hidden=True)
-        return
-
-    channel: d.VoiceChannel = voice.channel
-
-    permissions: d.Permissions = ctx.author.permissions_in(channel=channel)
-    if not permissions.deafen_members:
-        await ctx.send(content="⚠️ You don't have the _Deafen Members_ permission.", hidden=True)
-        return
-
-    members: list[d.Member] = channel.members
-    for member in members:
-        await member.edit(deafen=False)
-
-    await ctx.send(content="🔊 You haven't heard the last of me.")
+        await ctx.send(content="🔊 Speak your last.")
 
 
-print("Running bot...")
-bot.run(os.environ["DISCORD_BOT_TOKEN"])
+    @sbot.slash(name="deafen", description="Deafen all members in the voice channel you're in.")
+    async def _deafen(ctx: ds.SlashContext):
+        voice: t.Optional[d.VoiceState] = ctx.author.voice
+        if voice is None:
+            await ctx.send(content="⚠️ You're not in a voice channel.", hidden=True)
+            return
+
+        channel: d.VoiceChannel = voice.channel
+
+        permissions: d.Permissions = ctx.author.permissions_in(channel=channel)
+        if not permissions.deafen_members:
+            await ctx.send(content="⚠️ You don't have the _Deafen Members_ permission.", hidden=True)
+            return
+
+        members: list[d.Member] = channel.members
+        for member in members:
+            await member.edit(deafen=True)
+
+        await ctx.send(content="🔇 Hear not.")
+
+
+    @sbot.slash(name="undeafen", description="Undeafen all members in the voice channel you're in.")
+    async def _undeafen(ctx: ds.SlashContext):
+        voice: t.Optional[d.VoiceState] = ctx.author.voice
+        if voice is None:
+            await ctx.send(content="⚠️ You're not in a voice channel.", hidden=True)
+            return
+
+        channel: d.VoiceChannel = voice.channel
+
+        permissions: d.Permissions = ctx.author.permissions_in(channel=channel)
+        if not permissions.deafen_members:
+            await ctx.send(content="⚠️ You don't have the _Deafen Members_ permission.", hidden=True)
+            return
+
+        members: list[d.Member] = channel.members
+        for member in members:
+            await member.edit(deafen=False)
+
+        await ctx.send(content="🔊 You haven't heard the last of me.")
+
+
+    print("Running bot...")
+    bot.run(os.environ["DISCORD_BOT_TOKEN"])
+
+
+if __name__ == "__main__":
+    main()
